f973a9be790c099b061e219fa463cca0b5f1c027
Roman Khassraf <roman@khassraf.at>
2021-11-12 16:27:09 +0100

    Setup a template

---
+++ template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import turtle
+
+# Setup
+screen = turtle.Screen()
+screen.setup(1024, 768)
+turtle.delay(0)
+
+fritz = turtle.Turtle(shape='turtle')
+fritz.color('red', 'green')
+fritz.speed(4)
+
+# Code
+
+
+# Loop
+turtle.mainloop()
